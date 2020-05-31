32efae2bf908690d6a5c0df9ccffa93f3954a0a7
EdisonBernal <37006043+EdisonBernal@users.noreply.github.com>
2020-05-31 15:10:18 -0500

    Renombre, y update
    -Get para listar personas

---
+++ src/routes/employee.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const mysqlConnection  = require('../database.js');
+
+// GET all Employees
+router.get('/', (req, res) => {
+  mysqlConnection.query('SELECT * FROM employee', (err, rows, fields) => {
+    if(!err) {
+      res.json(rows);
+    } else {
+      console.log(err);
+    }
+  });  
+});
+
+module.exports = router;
