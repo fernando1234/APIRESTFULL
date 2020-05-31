ac54b5ac390b7be59253198dcd52d038cbe9709a
faber12andres <37006067+faber12andres@users.noreply.github.com>
2020-05-31 15:16:26 -0500

    Update get
    -Listar 1 solo empleado

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -13,5 +13,16 @@ router.get('/', (req, res) => {
     }
   });  
 });
+//obtener 1 solo empleyee
+router.get('/:id', (req, res) => {
+  const { id } = req.params; 
+  mysqlConnection.query('SELECT * FROM employee WHERE id = ?', [id], (err, rows, fields) => {
+    if (!err) {
+      res.json(rows[0]);
+    } else {
+      console.log(err);
+    }
+  });
+});
 
 module.exports = router;
